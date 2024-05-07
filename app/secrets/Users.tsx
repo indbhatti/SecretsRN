d9d4e63bb05fc878b26fa2362c8b22bb8142ae5f
walruii <walruiix@gmail.com>
2024-05-08 01:22:52 +0530

    test prod apiurl

--- app/secrets/Users.tsx
+++ app/secrets/Users.tsx
@@ -1,5 +1,7 @@
 const getUsers = async () => {
-  const response = await fetch(`${process.env.API_URL || "http://localhost:3000"}/api/secrets` , {
+
+  const apiUrl = process.env.API_URL || "http://localhost:3000";
+  const response = await fetch(`${apiUrl}/api/secrets`, {
     cache: 'no-store',
     headers: {
       'Content-Type': 'application/json',
@@ -12,7 +14,7 @@ const getUsers = async () => {
 export default async function Users() {
   const users = await getUsers();
   return (<div>
-    {users.map((user: {_id: string, secret: string}) => {
+    {users.map((user: { _id: string, secret: string }) => {
       return <p key={user._id} className="secret-text rounded-3">{user.secret}</p>
     })}
   </div>
