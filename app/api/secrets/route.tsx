d307b0d3a8eb7d77743ac64d3fd5ffe30df9b865
walruii <walruiix@gmail.com>
2024-05-08 04:47:19 +0530

    prod test 3

--- app/api/secrets/route.tsx
+++ app/api/secrets/route.tsx
@@ -5,8 +5,16 @@ import connectMongo from '../../../middleware/mongooseconnect'
 export async function GET(request: NextRequest) {
   try {
     const connect = await connectMongo();
-    const users = await User.find({ 'secret': { $ne: null } })
-    return NextResponse.json({ users });
+    if (connect) {
+      const users = await User.find({ 'secret': { $ne: null } })
+      console.log(users);
+      return NextResponse.json({ users });
+    } else {
+      console.log('FAILED TO CONNECT TO MONGO');
+
+      // Return an error response
+      return NextResponse.json({ error: "Failed to connect to the database." }, { status: 500 });
+    }
   } catch (error) {
     return NextResponse.error();
   }
