a9d3a4a9132efc06ad35161a3262caffd5883875
walruii <walruiix@gmail.com>
2024-05-26 12:37:10 +0000

    fetch issue prod fix 2

--- app/secrets/Users.tsx
+++ app/secrets/Users.tsx
@@ -1,25 +1,37 @@
+import { useRouter } from 'next/router'
 import { UserType } from "../../models/user"
 
 const getUsers = async () => {
-  const uri = process.env.NEXTAUTH_URL as string;
-  console.log(uri)
-  const response = await fetch(`${uri}/api/secrets`, {
-    method: "POST",
-    cache: 'no-store',
-    headers: {
-      'Content-Type': 'application/json',
-    }
-  });
-  const data = await response.json();
-  return data.users;
-}
+  const router = useRouter();
+  try {
+    const response = await fetch(`${router.basePath}/api/secrets`, {
+      method: "POST",
+      cache: 'no-store',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+    const data = await response.json();
+    return data.users;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Failed to fetch users');
+  }
+};
+
 
 export default async function Users() {
-  const users : Array<UserType> = await getUsers();
-  return (<div>
-    {users.map((user: UserType) => {
-      return <p key={user._id} className="secret-text rounded-3">{user.secret}</p>
-    })}
-  </div>
-  );
-}
+  try {
+    const users: Array<UserType> = await getUsers();
+    return (
+      <div>
+        {users.map((user: UserType) => {
+          return <p key={user._id} className="secret-text rounded-3">{user.secret}</p>
+        })}
+      </div>
+    );
+  } catch (error) {
+    console.error(error);
+    return <div>Error: Failed to load users</div>;
+  }
+} 
